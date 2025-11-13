d3636a120c5389610f370cfc67a8703d6e1639de
Sam <samkunkle@icloud.com>
2025-11-13 15:36:54 -0500

    Initial Commit

---
+++ Group3FinalProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Group3FinalProject
+//
+//  Created by Kunkle, Samuel I. on 11/13/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        // edit
+    }
+
+
+}
+
